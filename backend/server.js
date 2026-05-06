aac23a40c5c3a223af16b32254c120fdec7d54f8
allenlobo31 <allenlobo05@gmail.com>
2026-05-06 10:21:16 +0530

    feat: add health check endpoint to confirm server status

--- backend/server.js
+++ backend/server.js
@@ -119,3 +119,7 @@ app.use((req, res, next) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.status(200).send('Server is up and running!');
+});
